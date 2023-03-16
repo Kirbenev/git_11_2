f58e95764818723b691f7a5a7ef9c81e1c9e12c2
Oleg <kirbenev@bk.ru>
2023-03-16 23:43:17 +0300

    add new function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,12 @@
 def get_upper(text):
 
+    """ makes text capital"""
+
     return text.upper()
+
+def get_first_letter_capital(text):
+
+    """ makes first letter capital"""
+
+    return text.title()
+
