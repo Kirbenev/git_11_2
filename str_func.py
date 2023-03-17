6f74df6eec0e568a544121d5b2a14354a420897f
Oleg <kirbenev@bk.ru>
2023-03-17 21:20:37 +0300

    new function and conflict

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def get_upper(text):
+    """ This func returns text CAPITAL letters """
     return text.upper()
 
+
+def get_first_letter_capital(text):
+    """ makes first letter capital"""
+    return text.title()
